0734ad0836956d8e3d342e484622c86a8dbbe7cd
nyxeki <nyxekicyrex@gmail.com>
2025-09-02 17:25:28 +0800

    feat: implement server-side service invocation via reflection

--- src/main/java/io/github/nyxeki/minirpcframework/provider/HelloServiceImpl.java
+++ src/main/java/io/github/nyxeki/minirpcframework/provider/HelloServiceImpl.java
@@ -5,7 +5,6 @@ import io.github.nyxeki.minirpcframework.api.HelloService;
 public class HelloServiceImpl implements HelloService {
     @Override
     public String sayHello(String name) {
-        System.out.println("服务端接收到客户端的请求 +" + name);
-        return "Hello from rpc service" + name;
+        return "Hello " + name;
     }
 }
