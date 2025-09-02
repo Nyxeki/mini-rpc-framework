06e172aebb00ba8b957dd8f9a243953ad901d602
nyxeki <nyxekicyrex@gmail.com>
2025-09-02 15:57:26 +0800

    feat: implement request serialization and transport using ObjectStream

--- src/main/java/io/github/nyxeki/minirpcframework/provider/RpcServer.java
+++ src/main/java/io/github/nyxeki/minirpcframework/provider/RpcServer.java
@@ -1,6 +1,10 @@
 package io.github.nyxeki.minirpcframework.provider;
 
+import io.github.nyxeki.minirpcframework.api.RpcRequest;
+
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -19,13 +23,23 @@ public class RpcServer {
                 Socket client = server.accept();
                 System.out.println("Accepted connection from " + client.getInetAddress());
                 new Thread(() -> {
-                    try {
+                    try (
+                        ObjectInputStream ois = new ObjectInputStream(client.getInputStream());
+                        ObjectOutputStream oos = new ObjectOutputStream(client.getOutputStream())
+                    ) {
                         System.out.println("Processing request in thread: " + Thread.currentThread().getName());
+                        RpcRequest rpcRequest = (RpcRequest) ois.readObject();
+                        System.out.println("Received request from client: " + rpcRequest);
+
+                        // TODO: Call the real service method according to the content of the request.
+
+                    } catch (IOException | ClassNotFoundException e) {
+                        System.err.println("Error processing request: " + e.getMessage());
                     } finally {
                         try {
                             client.close();
                         } catch (IOException e) {
-                            System.err.println("Error closing client socket: " + e.getMessage());
+                            System.err.println("Error closing client: " + e.getMessage());
                         }
                     }
                 }).start();
